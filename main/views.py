8f8ad1a1797b7b91a3cbde5834371f09d47a6e0f
humam <humam.alabib@gmail.com>
2023-09-13 11:59:55 +0700

    terakhir

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'applications': 'storehousepbp',
-        'name': 'Humam Al Labib',
-        'class': 'PBP F'
+        'applications': 'storehousepbp', # isikan dengan nama aplikasi sendiri
+        'name': 'Humam Al Labib', #isikan dengan nama sendiri
+        'class': 'PBP F' # isikan dengan kelas kalian sendiri
     }
 
     return render(request, "main.html", context)
